476cade36fe0ef3e21c9dce9083670619974a883
wanjirungari <wanjirungari2@gmail.com>
2025-06-09 02:36:07 +0300

    check for alien colors with if and ilif statememnts

---
+++ exercises/condition1.py
@@ -0,0 +1,27 @@
+alienColors = ['yellow', 'green', 'red']
+
+def checkColor():
+    while True: 
+
+        color = input("Guess the alien's color or type q to quit:"
+        "")
+        if color.lower() == 'q':
+            print('Bye Player!')
+            break
+        if color not in alienColors:
+                print('color unknown!')
+        elif  color.lower() == 'green':
+                print("you earned five points!")
+        elif color.lower() == 'yellow':
+                print('you earned ten points!')
+        elif color.lower() == 'red':
+                print('you earned fifteen points!')
+                break
+        else:
+                print('Wrong answer!')
+
+
+
+
+checkColor()
+
